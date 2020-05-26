1082a419ea2e846c17ba988f665e7f12e67ffd7b
diksha1720 <dikshabharti39@gmail.com>
2020-05-26 20:16:54 +0530

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          title: Center(child: Text('ASK ME ANYTHING')),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: askme(),
+      ),
+    ),
+  );
+}
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+class askme extends StatefulWidget {
+  @override
+  _askmeState createState() => _askmeState();
+}
+
+class _askmeState extends State<askme> {
+  int imageno=1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child:Row(
+          children:<Widget>[
+            Expanded(
+              child:FlatButton(
+                onPressed:(){
+                  setState(() {
+                    imageno=Random().nextInt(5)+1;
+                  });
+              },
+                  child:Image.asset('images/ball$imageno.png'),
+
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
